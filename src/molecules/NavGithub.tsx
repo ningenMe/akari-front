8f2045ff87b6ddf40305b7d36a674a16fcb3a672
ningenMe <amohkuru@gmail.com>
2021-07-08 21:26:54 +0900

    [add] header

---
+++ src/molecules/NavGithub.tsx
@@ -0,0 +1,10 @@
+import { Nav,NavDropdown } from 'react-bootstrap';
+
+export const NavBlog = () => {
+    return (
+        <Nav>
+            <Nav.Link href="https://github.com/ningenMe">GitHub</Nav.Link>
+        </Nav>
+    );
+}
+export default NavBlog
