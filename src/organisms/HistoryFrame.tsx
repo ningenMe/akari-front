ac2b634dcbe06bacd00e1d290b3b5641151bcc91
ningenMe <amohkuru@gmail.com>
2021-07-12 00:58:00 +0900

    [modify]

--- src/organisms/HistoryFrame.tsx
+++ src/organisms/HistoryFrame.tsx
@@ -2,14 +2,33 @@ import {Grid,Typography,Card,CardContent} from '@material-ui/core'
 import {FrameTitle} from 'atoms/FrameTitle'
 import {CardStyle} from 'styles/CardStyle'
 import {histories} from 'constants/Histories'
+import {OptionalHref} from 'atoms/OptionalHref'
+import {HistoryContent} from 'interfaces/HistoryContent'
 
 export const HistoryFrame = () => {    
+
+
+    const innerContents = (historyContents:ReadonlyArray<HistoryContent>) => {
+        return historyContents.map((historyContent) => 
+        <>
+            <Grid item xs={1}>
+            </Grid>
+            <Grid item xs={11}>
+                {"・"}
+                <OptionalHref body={historyContent.body} href={historyContent.href}/>                
+            </Grid>
+        </>
+    );}
+
     const contents = histories.map((history) =>
-        <Grid item xs={12} sm={4}>
-            <Typography variant="body2" component="p">
+        <>
+            <Typography variant="body1" component="p">
                 {history.yearFrom}-{history.yearTo}: {history.body}
             </Typography>
-        </Grid>
+            <Grid container>
+                {innerContents(history.contents)}
+            </Grid>
+        </>
     );
 
     return (
