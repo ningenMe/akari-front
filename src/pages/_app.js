27a93050ceba41d2368e360bc25e91898c29d63a
ningenMe <amohkuru@gmail.com>
2021-07-12 00:04:38 +0900

    [modify]

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,12 @@ import 'styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Header} from 'organisms/Header'
 import {Footer} from 'organisms/Footer'
+import {HtmlHead} from 'organisms/HtmlHead'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <div>
+      <HtmlHead />
       <Header/>
       <Component {...pageProps} />
       <Footer/>
