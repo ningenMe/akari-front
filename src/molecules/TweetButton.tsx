8d0c6fef64de4f7b17d81d77b530c1e219013205
ningenMe <amohkuru@gmail.com>
2021-07-17 03:37:42 +0900

    [modify]

---
+++ src/molecules/TweetButton.tsx
@@ -0,0 +1,9 @@
+import { TwitterIcon, TwitterShareButton } from 'react-share'
+
+export const TweetButton = ({ text, url }: {text: string, url: string}) => {
+    return (
+        <TwitterShareButton url={url} title={text}>
+            <TwitterIcon size={50} round={true} />
+        </TwitterShareButton>
+    )
+}
