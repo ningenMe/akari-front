c9781c11e424519d378a3ef60220f9a9b63ce95c
ningenMe <amohkuru@gmail.com>
2021-08-18 03:21:36 +0900

    [fix]

--- src/molecules/BlogCard.tsx
+++ src/molecules/BlogCard.tsx
@@ -1,7 +1,7 @@
 import {Card,CardActionArea,CardContent,Typography,Grid,Box} from '@material-ui/core';
 import {Blog} from 'interfaces/Blog'
 import {CardStyle} from 'styles/CardStyle'
-import {blogIconPath} from 'atoms/BlogIconPath'
+import {blogIconPath} from 'atoms/blogIconPath'
 
 export const BlogCard = ({blog}: {blog:Blog}) => {
     return (
