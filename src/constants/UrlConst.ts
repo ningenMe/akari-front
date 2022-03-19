347eabf086448c1635d3003e05f472ce06331ab4
ningenMe <amohkuru@gmail.com>
2022-03-20 03:32:10 +0900

    [fix]

--- src/constants/UrlConst.ts
+++ src/constants/UrlConst.ts
@@ -1,6 +1,7 @@
 export class UrlConst {
   static readonly NINGENME_NET: string = 'https://ningenme.net'
-  static readonly API_NINGENME_NET: string = 'https://api.ningenme.net'
+  // static readonly API_NINGENME_NET: string = 'https://api.ningenme.net'
+  static readonly API_NINGENME_NET: string = 'https://localhost'
   static readonly GITHUB: string = 'https://github.com/ningenMe'
   static readonly TWITTER: string = 'https://twitter.com/ningenMe'
   static readonly ATCODER: string = 'https://atcoder.jp/users/ningenMe'
