b3e20e0d195ce901a1e3d841685868095bf0c9fe
ningenMe <amohkuru@gmail.com>
2022-05-04 11:01:07 +0900

    [add] header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,28 +2,46 @@ import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { LinkConst } from '../../constants/LinkConst'
 import { Link } from '../../interfaces/Link'
 import styles from './Header.module.scss'
+import React, { useState } from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { Box } from '@mui/system'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header = () => {
   return (
-    <Navbar bg='dark' variant='dark' expand='lg'>
+    <Navbar bg='primary' variant='dark' expand='lg'>
       <Navbar.Brand href={LinkConst.NINGENME_NET.href} className={styles.brand}>{LinkConst.NINGENME_NET.name}</Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' className={styles.toggle}/>
       <Navbar.Collapse id='basic-navbar-nav' className={styles.collapse} >
         <Nav>
-          <Nav.Link href={LinkConst.GITHUB.href}>{LinkConst.GITHUB.name}</Nav.Link>
+          <div>
+            <GitHubIcon className={styles.githubIcon} />
+            <Nav.Link href={LinkConst.GITHUB.href}>{LinkConst.GITHUB.name}</Nav.Link>
+          </div>
         </Nav>
         <Nav>
           <Nav.Link href={LinkConst.TWITTER.href}>{LinkConst.TWITTER.name}</Nav.Link>
         </Nav>
-        <Dropdown title='compro' links={LinkConst.COMPROS} />
-        <Dropdown title='blog' links={LinkConst.BLOGS} />
+        <Tmp title='compro' links={LinkConst.COMPROS} />
+        <Tmp title='blog' links={LinkConst.BLOGS} />
       </Navbar.Collapse>
     </Navbar>
   )
 }
 
-
-export const Dropdown = ({ title, links }: { title: string, links: ReadonlyArray<Link> }) => {
+export const Tmp = ({ title, links }: { title: string, links: ReadonlyArray<Link> }) => {
   const dropdowns = links.map((link) =>
     <NavDropdown.Item target='_blank' href={link.href} key={link.name}>{link.name}</NavDropdown.Item>,
   )
@@ -36,3 +54,109 @@ export const Dropdown = ({ title, links }: { title: string, links: ReadonlyArray
   )
 }
 
+const pages = ['Products', 'Pricing', 'Blog'];
+
+const Dropdown = ({ title, links }: { title: string, links: ReadonlyArray<Link> }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const onOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const onClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button onClick={onOpen} className={styles.button}>
+        {title}
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={onClose}
+      >
+        {links.map((link) => (
+          <MenuItem key={link.name}>
+            <a href={link.href}>
+              {link.name}
+            </a>
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
+
+export const ResponsiveAppBar = () => {
+
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+
+
+  return (
+    <AppBar position="static" className={styles.appbar}>
+      <Toolbar disableGutters>
+        <a href={LinkConst.NINGENME_NET.href} className={styles.brand}>
+          {LinkConst.NINGENME_NET.name}
+        </a>
+
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page}>
+                {page}
+              </MenuItem>
+            ))}
+            <Dropdown title={"compro"} links={LinkConst.COMPROS} />
+            <Dropdown title={"blog"} links={LinkConst.BLOGS}/>
+          </Menu>
+        </Box>
+
+        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+            >
+              {page}
+            </Button>
+          ))}
+          <Dropdown title={"compro"} links={LinkConst.COMPROS}/>
+          <Dropdown title={"blog"} links={LinkConst.BLOGS}/>
+        </Box>
+
+      </Toolbar>
+    </AppBar>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import { HtmlHead } from 'organisms/NingenmeNetTemplateOrganism'
 import { Home } from '../../components/templates/Home'
 import { Footer } from '../../components/organisms/Footer'
-import { Header } from '../../components/organisms/Header'
+import { Header, ResponsiveAppBar } from '../../components/organisms/Header'
 
 export const Index = () => {
   return (
     <>
       <HtmlHead />
+      <ResponsiveAppBar />
       <Header />
       <Home />
       <Footer />
