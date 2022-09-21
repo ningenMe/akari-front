1b7233ba4fa311bde90ca666fc0508f67d806b6d
ningenMe <amohkuru@gmail.com>
2022-09-22 04:29:51 +0900

    [fix]

---
+++ src/components/organisms/contribution/Contribution.tsx
@@ -0,0 +1,19 @@
+import { Container } from '@mui/material'
+import { Title } from '../../atoms/home/Title'
+import React from 'react'
+
+export const Contribution = () => {
+
+  // const x = async () => {
+  //   return ninaApiHealthClient.get(new Empty(), null)
+  //     .then(res => res.getMessage())
+  //     .catch(err => console.log(err))
+  // }
+
+  return (
+    <Container>
+      <Title title='Contribtuion' />
+      {/*{x}*/}
+    </Container>
+  )
+}
