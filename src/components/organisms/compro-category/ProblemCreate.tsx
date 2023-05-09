cdd0581da7ba7c892d32a08eaea1c970410bdc8e
ningenMe <amohkuru@gmail.com>
2023-05-10 01:01:12 +0900

    [fix]

--- src/components/organisms/compro-category/ProblemCreate.tsx
+++ src/components/organisms/compro-category/ProblemCreate.tsx
@@ -5,28 +5,30 @@ import { miikoApiMiikoServiceClient } from 'repository/MiikoApiRepository'
 import {
   Category,
   CategoryListGetRequest,
-  CategoryListGetResponse, Problem,
+  CategoryListGetResponse,
+  Problem,
   ProblemPostRequest,
   Tag,
   Topic,
 } from 'miiko-api/proto/gen_ts/v1/miiko_pb'
 import styles from './ProblemManage.module.scss'
-import { TagEditButton, UpsertButton } from '../../atoms/Button'
-import { CustomLinkCard } from '../CustomCard'
+import { PageTextCard, TagLinkCard } from '../../atoms/compro-category/Card'
+import { PathConst } from '../../../constants/Const'
+import { DeleteButton, UpsertButton } from '../../atoms/compro-category/Button'
 
 export const ProblemCreate = (): JSX.Element => {
 
-  const [url, setUrl] = useState <string>('')
-  const [problemDisplayName, setProblemDisplayName] = useState <string>('')
-  const [estimation, setEstimation] = useState <number>(0)
-  const [tagList, setTagList] = useState <Tag[]>([])
+  const [url, setUrl] = useState<string>('')
+  const [problemDisplayName, setProblemDisplayName] = useState<string>('')
+  const [estimation, setEstimation] = useState<number>(0)
+  const [tagList, setTagList] = useState<Tag[]>([])
 
-  const [categoryList, setCategoryList] = useState < Category[]>([])
-  const [selectedCategory, setSelectedCategory] = useState < Category>()
+  const [categoryList, setCategoryList] = useState<Category[]>([])
+  const [selectedCategory, setSelectedCategory] = useState<Category>()
 
   const categoryGet = async () => {
     const categoryListGetRequest = new CategoryListGetRequest({
-      isRequiredTopic: true
+      isRequiredTopic: true,
     })
 
     const categoryListGetResponse = await miikoApiMiikoServiceClient.categoryListGet(categoryListGetRequest) as CategoryListGetResponse
@@ -37,24 +39,6 @@ export const ProblemCreate = (): JSX.Element => {
     categoryGet()
   }, [])
 
-  const getTagCardList = (tagList: Tag[]) => {
-    return tagList
-      .map((it) =>
-        <TagEditButton name={it.topicDisplayName} key={it.topicId} onClick={() => handleClickSelectedTag(it)}/>
-      )
-  }
-
-  const handleChangeSelectedCategory = (event: SelectChangeEvent) => {
-    const tmp = JSON.parse(event.target.value) as Category
-    setSelectedCategory(tmp)
-  };
-  const handleChangeSelectedTopic = (event: SelectChangeEvent) => {
-    const tmp = JSON.parse(event.target.value) as Topic
-    setTagList((list) => [new Tag({topicId: tmp.topicId, topicDisplayName: tmp.topicDisplayName})].concat(list))
-  };
-  const handleClickSelectedTag = (tag: Tag) => {
-    setTagList((list) => list.filter(it => (it.topicId != tag.topicId)))
-  };
 
   const upsertClick = async () => {
     const request = new ProblemPostRequest()
@@ -63,34 +47,70 @@ export const ProblemCreate = (): JSX.Element => {
         url: url,
         problemDisplayName: problemDisplayName,
         estimation: estimation,
-        tagList: tagList
+        tagList: tagList,
       })
     }
     await miikoApiMiikoServiceClient.problemPost(request)
   }
 
+  const handleChangeSelectedCategory = (event: SelectChangeEvent) => {
+    const tmp = JSON.parse(event.target.value) as Category
+    setSelectedCategory(tmp)
+  }
+  const handleChangeSelectedTopic = (event: SelectChangeEvent) => {
+    const tmp = JSON.parse(event.target.value) as Topic
+    setTagList((list) => [new Tag({ topicId: tmp.topicId, topicDisplayName: tmp.topicDisplayName })].concat(list))
+  }
+  const handleClickSelectedTag = (tag: Tag) => {
+    setTagList((list) => list.filter(it => (it.topicId != tag.topicId)))
+  }
+
+  const getTagCardList = (tagList: Tag[]) => {
+    return tagList
+      .map((it) =>
+        <div key={it.topicId}>
+          <TagLinkCard
+            href={PathConst.COMPRO_CATEGORY_TOPIC_PROBLEM(it.topicId)}
+            topicDisplayName={it.topicDisplayName}
+          />
+          <DeleteButton
+            name='delete'
+            onClick={() => handleClickSelectedTag(it)}
+          />
+        </div>,
+      )
+  }
+
   return (
     <Container>
-      {/* TODO ここの説明文にcssを当てる */}
-      <Typography variant='body2'>problem 新規作成</Typography>
+      <PageTextCard>
+        <Typography variant='body2'>problem create</Typography>
+      </PageTextCard>
+
       <FormControl fullWidth className={styles.wrapper}>
         <TextField
           label='url'
-          onChange={(event) => {setUrl(event.target.value)}}
+          onChange={(event) => {
+            setUrl(event.target.value)
+          }}
           value={url}
           className={styles.textfield}
         />
         <TextField
           label='problemDisplayName'
-          onChange={(event) => {setProblemDisplayName(event.target.value)}}
+          onChange={(event) => {
+            setProblemDisplayName(event.target.value)
+          }}
           value={problemDisplayName}
           className={styles.textfield}
         />
         <TextField
-          type="number"
+          type='number'
           inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
           label='estimation'
-          onChange={(event) => {setEstimation(parseInt(event.target.value))}}
+          onChange={(event) => {
+            setEstimation(parseInt(event.target.value))
+          }}
           value={estimation}
           className={styles.textfield}
         />
@@ -102,7 +122,7 @@ export const ProblemCreate = (): JSX.Element => {
           {categoryList.map((it) =>
             <MenuItem key={it.categoryId} value={JSON.stringify(it)}>
               {it.categoryDisplayName}
-            </MenuItem>
+            </MenuItem>,
           )}
         </Select>
         <Select
@@ -112,18 +132,13 @@ export const ProblemCreate = (): JSX.Element => {
           {selectedCategory?.topicList.map((it) =>
             <MenuItem key={it.topicId} value={JSON.stringify(it)}>
               {it.topicDisplayName}
-            </MenuItem>
+            </MenuItem>,
           )}
         </Select>
       </FormControl>
-      <CustomLinkCard href={url}>
-        <div>{problemDisplayName}</div>
-        <div>{url}</div>
-        <div>{estimation}</div>
-      </CustomLinkCard>
       {getTagCardList(tagList)}
       <div className={styles.buttongrid}>
-        <UpsertButton onClick={upsertClick} />
+        <UpsertButton name='problem upsert' onClick={upsertClick} />
       </div>
 
     </Container>
