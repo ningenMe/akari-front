c807c8b4585b03ec7d5e125b363bc0b726daeeec
ningenMe <amohkuru@gmail.com>
2023-12-28 04:17:47 +0900

    [fix]

--- src/components/atoms/blogIconPath.ts
+++ src/components/atoms/blogIconPath.ts
@@ -1,9 +1,6 @@
-import { BlogType, BlogTypeConst } from 'interfaces/Blog'
-
-export const blogIconPath = (blogType: BlogType): string => {
-  if (blogType === BlogTypeConst.HATENA) return 'hatena.svg'
-  if (blogType === BlogTypeConst.QIITA) return 'qiita.png'
-  if (blogType === BlogTypeConst.AMEBA) return 'ningenme.png'
-  if (blogType === BlogTypeConst.DIARY) return 'dokata.png'
+export const blogIconPath = (blogType: string): string => {
+  if (blogType === "HATENA") return 'hatena.svg'
+  if (blogType === "QIITA") return 'qiita.png'
+  if (blogType === "AMEBA") return 'ningenme.png'
   return 'ningenme.png'
 }
--- src/components/organisms/home/RecentBlog.tsx
+++ src/components/organisms/home/RecentBlog.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from 'react'
 import { Empty } from 'google-protobuf/google/protobuf/empty_pb'
 import { suzuApiBlogServiceClient } from 'repository/SuzuApiRepository'
 import { Blog } from 'suzu-backend/api/proto/client/api/proto/suzu/v1/suzu_pb'
+import { Title } from 'components/atoms/Title'
+import { OptionalHref } from 'components/atoms/OptionalHref'
+import styles from './RecentBlog.module.scss'
+import fontStyles from 'styles/Font.module.scss'
 
 export const RecentBlog = () => {
 
@@ -11,19 +15,23 @@ export const RecentBlog = () => {
     () => {
       suzuApiBlogServiceClient.getBlog(new Empty(), null)
       .then(res => {
-        setBlogList(res.getBlogListList())
+        setBlogList(res.getBlogListList().slice(0, 5))
       })
       .catch(err => console.log(err))
     },
     [suzuApiBlogServiceClient])
-  const blogCardList = blogList.map(it => (
-    <div key={it.getUrl()}> 
-      {it.getBlogTitle()}
+
+  const blogCardList = blogList.map((blog, idx) => (
+    <div key={idx} className={fontStyles.body}> 
+      <span className={styles.date}>{blog.getDate()}</span>
+      {': '}
+      <OptionalHref body={blog.getBlogTitle()} href={blog.getUrl()} />
     </div>
   ));
 
   return (
     <>
+      <Title title='Blog' />
       {blogCardList}
     </>
   )
