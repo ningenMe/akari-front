9aa72f75158b0a6dd9fbcf7fb31009bd1ba71389
ningenMe <amohkuru@gmail.com>
2023-12-30 08:00:29 +0900

    [add] suzu health

--- src/components/organisms/system/System.tsx
+++ src/components/organisms/system/System.tsx
@@ -1,19 +1,22 @@
 import { Container } from '@mui/material'
-import { Title } from '../../atoms/Title'
+import { Title } from 'components/atoms/Title'
 import React, { useEffect, useState } from 'react'
 import styles from './System.module.scss'
 import { CustomLinkCard, CustomNormalCard } from '../CustomCard'
-import { ninaApiHealthServiceClient } from '../../../repository/NinaApiRepository'
-import { PathConst, UrlConst } from '../../../constants/Const'
-import fontStyles from '../../../styles/Font.module.scss'
-import { kiwaApiHealthcheckClient } from '../../../repository/KiwaApiRepository'
-import { miikoApiHealthServiceClient } from '../../../repository/MiikoApiRepository'
+import { ninaApiHealthServiceClient } from 'repository/NinaApiRepository'
+import { PathConst, UrlConst } from 'constants/Const'
+import fontStyles from 'styles/Font.module.scss'
+import { kiwaApiHealthcheckClient } from 'repository/KiwaApiRepository'
+import { miikoApiHealthServiceClient } from 'repository/MiikoApiRepository'
+import { suzuApiBlogServiceClient } from 'repository/SuzuApiRepository'
+import { Empty } from 'google-protobuf/google/protobuf/empty_pb'
 
 export const System = (): JSX.Element => {
 
   const [ninaApiHealth, setNinaApiHealth] = useState('')
   const [kiwaApiHealth, setKiwaApiHealth] = useState('')
   const [miikoApiHealth, setMiikoApiHealth] = useState('')
+  const [suzuApiHealth, setSuzuApiHealth] = useState('')
 
   useEffect(() => {
     ninaApiHealthServiceClient.check({})
@@ -33,6 +36,12 @@ export const System = (): JSX.Element => {
       .catch(err => {
         console.log(err)
         setMiikoApiHealth('not found')
+      })
+    suzuApiBlogServiceClient.getHealth(new Empty(), null)
+      .then(() => setSuzuApiHealth('ok'))
+      .catch(err => {
+        console.log(err)
+        setSuzuApiHealth('not found')
       })
   }, [])
 
@@ -67,6 +76,16 @@ export const System = (): JSX.Element => {
             </h5>
             <p className={fontStyles.body}>
               healthcheck: {miikoApiHealth}
+            </p>
+          </div>
+        </CustomNormalCard>
+        <CustomNormalCard>
+          <div>
+            <h5 className={styles.title}>
+              {UrlConst.SUZU_API}
+            </h5>
+            <p className={fontStyles.body}>
+              healthcheck: {suzuApiHealth}
             </p>
           </div>
         </CustomNormalCard>
--- src/repository/SuzuApiRepository.ts
+++ src/repository/SuzuApiRepository.ts
@@ -1,10 +1,4 @@
 import { UrlConst } from 'constants/Const'
 import { BlogServiceClient } from 'suzu-backend/api/proto/client/api/proto/suzu/v1/SuzuServiceClientPb'
-import { createConnectTransport } from '@bufbuild/connect-web'
-import { createPromiseClient } from '@bufbuild/connect'
 
 export const suzuApiBlogServiceClient = new BlogServiceClient(UrlConst.SUZU_API)
-
-const transport = createConnectTransport({
-    baseUrl: UrlConst.SUZU_API
-})
