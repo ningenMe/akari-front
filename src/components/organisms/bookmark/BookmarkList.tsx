fef7e14c59214f48ed13621a5fba86499a4e7b40
ningenMe <amohkuru@gmail.com>
2024-02-11 03:05:41 +0900

    [add] bookmark

--- src/components/organisms/bookmark/BookmarkList.tsx
+++ src/components/organisms/bookmark/BookmarkList.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState } from 'react'
-import { suzuApiBlogServiceClient } from 'repository/SuzuApiRepository'
 import { BookmarkDirectory } from 'roman-api/client/src/generated'
-import { Container, List } from '@mui/material'
-import { BlogChip } from 'components/atoms/blog/BlogChip'
+import { Container, List, ListItem, ListItemText } from '@mui/material'
 import { romanBookmarkApiClient } from 'repository/RomanApiRepository'
+import { CustomNormalCard } from 'components/organisms/CustomCard'
+import { SubTitle } from 'components/atoms/Title'
+import fontStyles from 'styles/Font.module.scss'
+import styles from './BookmarkList.module.scss'
 
 export const BookmarkList = (): JSX.Element => {
 
@@ -19,10 +21,34 @@ export const BookmarkList = (): JSX.Element => {
     },
     [romanBookmarkApiClient])
 
-  const bookmarkCardList = bookmarkDirectoryList.map((it, idx) => (<div>{it.title}</div>));
+  const bookmarkCardList = bookmarkDirectoryList.map((bookmarkDirectory, idx) => (
+    <CustomNormalCard key={idx}>
+      <span className={styles.directoryTitle}>{bookmarkDirectory.title}</span>   
+
+      {bookmarkDirectory.bookmarkList.map(bookmark => {
+        return (
+          <ListItem disablePadding className={styles.bookmarkItem} key={bookmark.url}>
+            <ListItemText className={fontStyles.body}>
+              <a href={bookmark.url} className={styles.bookmarkTitle}>{bookmark.title}</a>
+            </ListItemText>
+          </ListItem>
+        );
+      })}
+    </CustomNormalCard>)
+  );
 
   return (
     <Container>
+      <SubTitle title={'favorite dev-bookmark'} />
+      <CustomNormalCard>
+
+      <p className={fontStyles.body}>
+        自分用の開発途中に見た記事まとめ。
+      </p>
+      <p className={fontStyles.body}>
+        自分用のまとめとして他の方の記事を引用させていただいています。このサイト自体に営利目的はないですが、もし気に障る場合はtwitter経由で連絡ください。
+      </p>
+      </CustomNormalCard>
       <List>
         {bookmarkCardList}
       </List>
